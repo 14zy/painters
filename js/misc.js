f4220339f2523cce1caefee8d0c34ac66bf12350
14zy <just14zy@gmail.com>
2014-10-08 13:26:54 +0400

    speed?

---
+++ js/misc.js
@@ -0,0 +1,35 @@
+//vk
+VK.Widgets.Group("vk_groups", {mode: 0, width: "360", height: "250", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 70435584);
+
+//fb
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "http://connect.facebook.net/en_GB/all.js#xfbml=1&appId=478531102278887";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+//<!-- Yandex.Metrika counter -->
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter24594722 = new Ya.Metrika({id:24594722,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
