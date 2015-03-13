f9038af79218ac144376245bcfe0c95b88779dbb
14zy <just14zy@gmail.com>
2015-03-13 13:44:05 +0300

    wipe

--- reader.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-
-for i in 1..56
-  link = JSON.parse(IO.read("painters/#{i}/data.json"))['link']['wikipedia']['en']
-  name = JSON.parse(IO.read("painters/#{i}/data.json"))['name']['en']
-  id = JSON.parse(IO.read("painters/#{i}/data.json"))['id']
-  puts "<a target='_blank' href='#{link}'>#{id}) #{name}</a><br>"
-end
