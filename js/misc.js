6cc850fcf363c9f186df51bb8e8f4fedd2da65e8
14zy <just14zy@gmail.com>
2015-05-19 13:45:20 +0300

    boomstarter

--- js/misc.js
+++ js/misc.js
@@ -68,38 +68,90 @@ $('#mc-embedded-subscribe-form').submit(function () {
 
 //boomstarter
 
-// $( document ).ready(function() {
-//
-// 	window.promoImg = Math.floor(( Math.random() * 6 )) + 1;
-// 	window.promoTextId = Math.floor(( Math.random() * 6 )) + 1;
-//
-// 	switch(window.promoTextId) {
-// 	  case 1:
-// 		window.promoText = "Поддержите разработку мобильного приложения!";
-// 		break;
-// 	  case 2:
-//   		window.promoText = "Скоро будет мобильное приложение!";
-//   		break;
-// 	  case 3:
-//   		window.promoText = "Мобильное приложение Art Challenge";
-//   		break;
-// 	  case 4:
-//   		window.promoText = "Мы разрабатываем мобильное приложение!";
-//   		break;
-// 	  case 5:
-// 		window.promoText = "Господа, поддержите нас на Boomstarter";
-// 		break;
-// 	  case 6:
-// 		window.promoText = "Господа, мобильное приложение!";
-// 		break;
-//
-// 	};
-//
-// 	$( "#promo-text" ).text(window.promoText);
-// 	$( "#promo-img" ).attr("src","pics/promo/iphone" + window.promoImg + ".png")
-//
-// });
-//
-// $( "#promo-link" ).click(function() {
-//   yaCounter24594722.reachGoal('BOOMSTARTER-CLICK-1');
-// });
+$( document ).ready(function() {
+
+	window.promoImg = Math.floor(( Math.random() * 10 )) + 1;
+	window.promoTextId = Math.floor(( Math.random() * 8 )) + 1;
+	window.promoTextColorId = Math.floor(( Math.random() * 4 )) + 1;
+
+	switch(window.promoTextId) {
+	  case 1:
+		window.promoText = "Поддержите разработку мобильного приложения!";
+		break;
+	  case 2:
+  		window.promoText = "Скоро будет мобильное приложение!";
+  		break;
+	  case 3:
+  		window.promoText = "Мобильное приложение Art Challenge";
+  		break;
+	  case 4:
+  		window.promoText = "Мы разрабатываем мобильное приложение!";
+  		break;
+	  case 5:
+		window.promoText = "Господа, поддержите нас на Boomstarter";
+		break;
+	  case 6:
+		window.promoText = "Господа, мобильное приложение!";
+		break;
+	  case 7:
+		window.promoText = "Приложение не появится без Вашей помощи!";
+		break;
+	  case 8:
+		window.promoText = "Господа, помогите нам с приложением!";
+		break;
+	};
+	
+	switch(window.promoTextColorId) {
+	  case 1:
+		window.promoTextColor = "black";
+		break;
+	  case 2:
+		window.promoTextColor = "#a94442"; //red
+  		break;
+	  case 3:
+		window.promoTextColor = "#337ab7"; //blue
+  		break;
+	  case 4:
+		window.promoTextColor = "#15cb63"; //green
+		break;
+
+	};
+
+	$( "#promo-text" ).text(window.promoText);
+	$( "#promo-img" ).attr("src","pics/promo/iphone" + window.promoImg + ".png");
+	$( "#promo-link" ).css( "color", window.promoTextColor );
+
+});
+
+$( "#promo-link" ).click(function() {
+	
+	var currentDate = new Date()
+	
+	var data = "click=" 
+	+ currentDate.getDate() 
+	+ "." + currentDate.getMonth()+1 
+	+ "." + currentDate.getFullYear() 
+	+ "; " + currentDate.getHours() 
+	+ ":" + currentDate.getMinutes() 
+	+ "; " + window.promoTextId 
+	+ "; " + window.promoText 
+	+ "; " + window.promoImg
+	+ "; " + window.promoTextColor
+	+ "; " + window.promoTextColorId;
+		
+    $.ajax({
+        type: 'POST',
+        url: 'http://178.62.133.139/analytics/post.php',
+        data: data,
+        error: function()
+        {
+			console.log('error');
+        },
+        success: function(response)
+        {
+			console.log('success');
+        }});
+	
+	
+  yaCounter24594722.reachGoal('PROMO-CLICK');
+});
