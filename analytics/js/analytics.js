8602b5644f01a0af8d5162d138007121d551dd9b
14zy <just14zy@gmail.com>
2015-06-18 02:37:12 +0300

    disable promo

--- analytics/js/analytics.js
+++ analytics/js/analytics.js
@@ -33,84 +33,84 @@ function recordData(){
 };
 
 //// Выбираем промо в зависимости от региона
-setTimeout(function () {
-	
-switch (2) { //geoplugin_region()
-	case "Rostov":
-		window.promoImgId = [1,2][Math.floor(Math.random() * 2)];
-		window.promoImg = "creativespace/" + window.promoImgId + ".jpg";
-
-		window.promoTextId = Math.floor(( Math.random() * 2 )) + 1;
-		switch(window.promoTextId) {
-		  case 1:
-			  window.promoText = "Выставка \"Натяжение\""; // «Натяжение»
-			break;
-		  case 2:
-			window.promoText = "Инсталяции Крестовой Катерины";
-			break;
-		};
-
-		window.promoTextSub = "Creative Space, Ростов-на-Дону";
-		window.promoTextColor = "black";
-		window.promoTextHoverColor = "red";
-		window.promoLink = "https://www.facebook.com/groups/CreativeSpace.PRO/permalink/861037833969038/"
-		break;
-	default:
-		//Реклама мобильного приложения
-		window.promoImgId = [1,3,4,6,10][Math.floor(Math.random() * 5)];
-		window.promoImg = "mobileapp/iphone" + window.promoImgId + ".png";
-		window.promoTextId = Math.floor(( Math.random() * 5 )) + 1;
-		window.promoTextColorId = Math.floor(( Math.random() * 2 )) + 1;
-		window.promoTextSub = "";
-		window.promoTextHoverColor = "black";
-		window.promoLink = "https://boomstarter.ru/projects/90794/art_challenge_-_igra_na_znanie_populyarnyh_hudozhnikov"
-		
-		switch(window.promoTextId) {
-		  case 1:
-			window.promoText = "Поддержите разработку мобильного приложения!";
-			break;
-		  case 2:
-			window.promoText = "Мобильное приложение Art Challenge";
-			break;
-		  case 3:
-			window.promoText = "Господа, мобильное приложение!";
-			break;
-		  case 4:
-			window.promoText = "Приложение не появится без Вашей помощи!";
-			break;
-		  case 5:
-			window.promoText = "Господа, помогите нам с приложением!";
-			break;
-		};
-
-		switch(window.promoTextColorId) {
-		  case 1:
-			window.promoTextColor = "black";
-			break;
-		  case 2:
-			window.promoTextColor = "#337ab7"; //blue
-			break;
-		};
-		// --Реклама мобильного приложения
-}; // switch
-
-//// Задаем значения верхнему промо
-$( "#promo-text" ).text(window.promoText);
-$( "#promo-text-sub" ).text(window.promoTextSub);
-$( "#promo-img" ).attr("src","pics/promo/" + window.promoImg); 
-$( "#promo-text" ).css( "color", window.promoTextColor );
-$( "#promo-link" ).attr("href", window.promoLink);
-$( "#promo-link" ).hover(
-  function() {
-	  $( "#promo-text" ).css( "color", window.promoTextHoverColor );
-	  $( "#promo-text" ).css( "text-decoration","underline" );
-  }, function() {
-	  $( "#promo-text" ).css( "color", window.promoTextColor );
-	  $( "#promo-text" ).css( "text-decoration","none" );
-  }
-);
-
-}, 1000);
+// setTimeout(function () {
+//
+// switch (2) { //geoplugin_region()
+// 	case "Rostov":
+// 		window.promoImgId = [1,2][Math.floor(Math.random() * 2)];
+// 		window.promoImg = "creativespace/" + window.promoImgId + ".jpg";
+//
+// 		window.promoTextId = Math.floor(( Math.random() * 2 )) + 1;
+// 		switch(window.promoTextId) {
+// 		  case 1:
+// 			  window.promoText = "Выставка \"Натяжение\""; // «Натяжение»
+// 			break;
+// 		  case 2:
+// 			window.promoText = "Инсталяции Крестовой Катерины";
+// 			break;
+// 		};
+//
+// 		window.promoTextSub = "Creative Space, Ростов-на-Дону";
+// 		window.promoTextColor = "black";
+// 		window.promoTextHoverColor = "red";
+// 		window.promoLink = "https://www.facebook.com/groups/CreativeSpace.PRO/permalink/861037833969038/"
+// 		break;
+// 	default:
+// 		//Реклама мобильного приложения
+// 		window.promoImgId = [1,3,4,6,10][Math.floor(Math.random() * 5)];
+// 		window.promoImg = "mobileapp/iphone" + window.promoImgId + ".png";
+// 		window.promoTextId = Math.floor(( Math.random() * 5 )) + 1;
+// 		window.promoTextColorId = Math.floor(( Math.random() * 2 )) + 1;
+// 		window.promoTextSub = "";
+// 		window.promoTextHoverColor = "black";
+// 		window.promoLink = "https://boomstarter.ru/projects/90794/art_challenge_-_igra_na_znanie_populyarnyh_hudozhnikov"
+//
+// 		switch(window.promoTextId) {
+// 		  case 1:
+// 			window.promoText = "Поддержите разработку мобильного приложения!";
+// 			break;
+// 		  case 2:
+// 			window.promoText = "Мобильное приложение Art Challenge";
+// 			break;
+// 		  case 3:
+// 			window.promoText = "Господа, мобильное приложение!";
+// 			break;
+// 		  case 4:
+// 			window.promoText = "Приложение не появится без Вашей помощи!";
+// 			break;
+// 		  case 5:
+// 			window.promoText = "Господа, помогите нам с приложением!";
+// 			break;
+// 		};
+//
+// 		switch(window.promoTextColorId) {
+// 		  case 1:
+// 			window.promoTextColor = "black";
+// 			break;
+// 		  case 2:
+// 			window.promoTextColor = "#337ab7"; //blue
+// 			break;
+// 		};
+// 		// --Реклама мобильного приложения
+// }; // switch
+//
+// //// Задаем значения верхнему промо
+// $( "#promo-text" ).text(window.promoText);
+// $( "#promo-text-sub" ).text(window.promoTextSub);
+// $( "#promo-img" ).attr("src","pics/promo/" + window.promoImg);
+// $( "#promo-text" ).css( "color", window.promoTextColor );
+// $( "#promo-link" ).attr("href", window.promoLink);
+// $( "#promo-link" ).hover(
+//   function() {
+// 	  $( "#promo-text" ).css( "color", window.promoTextHoverColor );
+// 	  $( "#promo-text" ).css( "text-decoration","underline" );
+//   }, function() {
+// 	  $( "#promo-text" ).css( "color", window.promoTextColor );
+// 	  $( "#promo-text" ).css( "text-decoration","none" );
+//   }
+// );
+//
+// }, 1000);
 
 $( "#promo-link" ).click(function() {
 	
--- js/misc.js
+++ js/misc.js
@@ -69,140 +69,140 @@ $('#mc-embedded-subscribe-form').submit(function () {
 //boomstarter
 
 
-$( document ).ready(function() {
+// $( document ).ready(function() {
 	
 	// Промо окно
-    var promoDiv = "\
-   	<div class='col-xs-4' style='padding-left: 5%'>\
-		<img class='img-responsive' src='pics/promo/bottom-banner14.png'>\
-	</div>\
-   	<div class='col-xs-8' style='text-align: justify; color: black; padding-right: 5%'>\
-		<br>\
-		<p>Мы искренне благодарим всех, кто поддержал наш проект!</p>\
-		<p>Очень приятно, что среди Вас, господа, так много тех, кто готов поддерживать Art Challenge.</p>\
-		<p>Список меценатов скоро появится в игре и навсегда останется в ней!</p>\
-		<p>Если Вам нравится Art Challenge - у Вас остается всего несколько часов, чтобы помочь проекту.</p>\
-		<p>Даже малая сумма - 100 или 200 р. - значимый вклад в общее дело, и в конечном итоге - в культуру и искусство!</p>\
-		<p>Благодарим Вас, господа! Рады, что Вы с нами!</p>\
-		<br>\
-		<div style=''>\
-			<button id='promoBtnYes' class='btn btn-lg btn-success'>Поддержать Art Challenge</button>\
-			<button id='promoBtnNo' class='btn btn-lg btn-danger'>Мне не интересно</button>\
-		</div>\
-	</div>\
-	"
-	var date1 = new Date("6/18/2015");
-	var date2 = new Date();
-	var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-	var diffDays = Math.ceil(timeDiff / (1000 * 3600)) + 12; 
-	
-	// if (getCookie("promo") != 2) {
-	    window.msgPromo = new PNotify({
-	        title: "До конца кампании осталось " + diffDays + " часов!",
-	        text: promoDiv,
-	        type: 'note',
-	        hide: false,
-	        animate_speed: "normal",
-	        icon: false,
-	        addclass: "stack-promo",
-	        opacity: 0.95,
-	        buttons: {
-	          closer: false,
-	          sticker: false
-	        },
-	        history: {
-	          history: true,
-	          menu: false
-	        }
-	    });
-		
-	// 	setCookie('promo',2,360);
-	// };
-
-	$( "#promoBtnYes" ).click(function() {
-		window.msgPromo.remove(); 
-		window.open('https://boomstarter.ru/projects/90794/art_challenge_-_igra_na_znanie_populyarnyh_hudozhnikov', '_blank');
-        swal({
-          title: "Спасибо за Вашу поддержку!",
-          type: "success",
-        });
-		yaCounter24594722.reachGoal('PROMO-BTN-YES');
-	});
-
-
-	$( "#promoBtnNo" ).click(function() {
-		window.msgPromo.remove();
-		yaCounter24594722.reachGoal('PROMO-BTN-NO');
-	});
+	//     var promoDiv = "\
+	//    	<div class='col-xs-4' style='padding-left: 5%'>\
+	// 	<img class='img-responsive' src='pics/promo/bottom-banner14.png'>\
+	// </div>\
+	//    	<div class='col-xs-8' style='text-align: justify; color: black; padding-right: 5%'>\
+	// 	<br>\
+	// 	<p>Мы искренне благодарим всех, кто поддержал наш проект!</p>\
+	// 	<p>Очень приятно, что среди Вас, господа, так много тех, кто готов поддерживать Art Challenge.</p>\
+	// 	<p>Список меценатов скоро появится в игре и навсегда останется в ней!</p>\
+	// 	<p>Если Вам нравится Art Challenge - у Вас остается всего несколько часов, чтобы помочь проекту.</p>\
+	// 	<p>Даже малая сумма - 100 или 200 р. - значимый вклад в общее дело, и в конечном итоге - в культуру и искусство!</p>\
+	// 	<p>Благодарим Вас, господа! Рады, что Вы с нами!</p>\
+	// 	<br>\
+	// 	<div style=''>\
+	// 		<button id='promoBtnYes' class='btn btn-lg btn-success'>Поддержать Art Challenge</button>\
+	// 		<button id='promoBtnNo' class='btn btn-lg btn-danger'>Мне не интересно</button>\
+	// 	</div>\
+	// </div>\
+	// "
+	// var date1 = new Date("6/18/2015");
+	// var date2 = new Date();
+	// var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+	// var diffDays = Math.ceil(timeDiff / (1000 * 3600)) + 12;
+	//
+	// // if (getCookie("promo") != 2) {
+	//     window.msgPromo = new PNotify({
+	//         title: "До конца кампании осталось " + diffDays + " часов!",
+	//         text: promoDiv,
+	//         type: 'note',
+	//         hide: false,
+	//         animate_speed: "normal",
+	//         icon: false,
+	//         addclass: "stack-promo",
+	//         opacity: 0.95,
+	//         buttons: {
+	//           closer: false,
+	//           sticker: false
+	//         },
+	//         history: {
+	//           history: true,
+	//           menu: false
+	//         }
+	//     });
+	//
+	// // 	setCookie('promo',2,360);
+	// // };
+	//
+	// $( "#promoBtnYes" ).click(function() {
+	// 	window.msgPromo.remove();
+	// 	window.open('https://boomstarter.ru/projects/90794/art_challenge_-_igra_na_znanie_populyarnyh_hudozhnikov', '_blank');
+	//         swal({
+	//           title: "Спасибо за Вашу поддержку!",
+	//           type: "success",
+	//         });
+	// 	yaCounter24594722.reachGoal('PROMO-BTN-YES');
+	// });
+	//
+	//
+	// $( "#promoBtnNo" ).click(function() {
+	// 	window.msgPromo.remove();
+	// 	yaCounter24594722.reachGoal('PROMO-BTN-NO');
+	// });
 	// --Промо окно
 
-});
+// });
 
 
 // Промо баннер нижний
 
-window.promoBottomImg = [1,2,3,5,7,8,10,12,14][Math.floor(Math.random() * 9)];
-		window.promoBottomTextId = Math.floor(( Math.random() * 7 )) + 1;
-		switch(window.promoBottomTextId) {
-		  case 1:
-			window.promoBottomText = "Без Вас приложение не появится!";
-			break;
-		  case 2:
-			window.promoBottomText = "Народное финансирование мобильного приложения";
-			break;
-		  case 3:
-			window.promoBottomText = "Сбор средств на мобильное приложение";
-			break;
-		  case 4:
-			window.promoBottomText = "Господа, давайте сделаем мобильное приложение!";
-			break;
-		  case 5:
-			window.promoBottomText = "Мобильное приложение ArtChallenge";
-			break;
-		  case 6:
-			window.promoBottomText = "Господа, помогите нам с приложением!";
-			break;
-		  case 7:
-			window.promoBottomText = "Господа, мобильное приложение!";
-			break;
-		};
-		
-		$( "#promo-bottom-text" ).html(window.promoBottomText);
-		$( "#promo-bottom-img" ).attr("src","pics/promo/bottom-banner" + window.promoBottomImg + ".png");
-		
-
-
-$( "#promo-bottom" ).hover(function() {
-	$( this).animate({ marginBottom: '-80px' }, 500);
-	$( "#promo-bottom p" ).css("display", "block");
-}, function() {
-	$( this ).animate({ marginBottom: '-440px' }, 500);
-	$( "#promo-bottom p" ).css("display", "none");
-  }
-);
-
-$( "#promo-bottom" ).click(function() {
-	window.open('https://boomstarter.ru/projects/90794/art_challenge_-_igra_na_znanie_populyarnyh_hudozhnikov', '_blank');
-	yaCounter24594722.reachGoal('PROMO-BOTTOM-CLICK');
-	
-	var currentDate = new Date()
-	
-	var data = "click=" 
-	+ getLocalTime(MOSCOW_OFF) 
-	+ "; " + window.promoBottomText 
-	+ "; " + window.promoBottomImg;
-    $.ajax({
-        type: 'POST',
-        url: 'http://178.62.133.139/analytics/mayak.php',
-        data: data,
-        error: function()
-        {
-			console.log('error');
-        },
-        success: function(response)
-        {
-			console.log('success');
-        }});
-	
-});
+// window.promoBottomImg = [1,2,3,5,7,8,10,12,14][Math.floor(Math.random() * 9)];
+// 		window.promoBottomTextId = Math.floor(( Math.random() * 7 )) + 1;
+// 		switch(window.promoBottomTextId) {
+// 		  case 1:
+// 			window.promoBottomText = "Без Вас приложение не появится!";
+// 			break;
+// 		  case 2:
+// 			window.promoBottomText = "Народное финансирование мобильного приложения";
+// 			break;
+// 		  case 3:
+// 			window.promoBottomText = "Сбор средств на мобильное приложение";
+// 			break;
+// 		  case 4:
+// 			window.promoBottomText = "Господа, давайте сделаем мобильное приложение!";
+// 			break;
+// 		  case 5:
+// 			window.promoBottomText = "Мобильное приложение ArtChallenge";
+// 			break;
+// 		  case 6:
+// 			window.promoBottomText = "Господа, помогите нам с приложением!";
+// 			break;
+// 		  case 7:
+// 			window.promoBottomText = "Господа, мобильное приложение!";
+// 			break;
+// 		};
+//
+// 		$( "#promo-bottom-text" ).html(window.promoBottomText);
+// 		$( "#promo-bottom-img" ).attr("src","pics/promo/bottom-banner" + window.promoBottomImg + ".png");
+//
+//
+//
+// $( "#promo-bottom" ).hover(function() {
+// 	$( this).animate({ marginBottom: '-80px' }, 500);
+// 	$( "#promo-bottom p" ).css("display", "block");
+// }, function() {
+// 	$( this ).animate({ marginBottom: '-440px' }, 500);
+// 	$( "#promo-bottom p" ).css("display", "none");
+//   }
+// );
+//
+// $( "#promo-bottom" ).click(function() {
+// 	window.open('https://boomstarter.ru/projects/90794/art_challenge_-_igra_na_znanie_populyarnyh_hudozhnikov', '_blank');
+// 	yaCounter24594722.reachGoal('PROMO-BOTTOM-CLICK');
+//
+// 	var currentDate = new Date()
+//
+// 	var data = "click="
+// 	+ getLocalTime(MOSCOW_OFF)
+// 	+ "; " + window.promoBottomText
+// 	+ "; " + window.promoBottomImg;
+//     $.ajax({
+//         type: 'POST',
+//         url: 'http://178.62.133.139/analytics/mayak.php',
+//         data: data,
+//         error: function()
+//         {
+// 			console.log('error');
+//         },
+//         success: function(response)
+//         {
+// 			console.log('success');
+//         }});
+//
+// });
 // --Промо баннер нижний
