4bdd84396f881ba05e75811311c761dff9eda875
Ruben Babaev <just14zy@gmail.com>
2017-02-27 00:55:52 +0300

    version

--- 2017/js/14functions.js
+++ 2017/js/14functions.js
@@ -46,3 +46,8 @@ jQuery.browser = {};
       jQuery.browser.version = RegExp.$1;
  }
 })();
+
+function shuffle(o) { //v1.0
+  for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
+}
--- 2017/js/app.js
+++ 2017/js/app.js
@@ -1,33 +1,48 @@
+var painters=[];
+var currentPainter="";
+var answers=[];
+var Lang = "en";
+var langDB = "";
+var scores = 0;
+
 $.couch.urlPrefix = "http://178.62.133.139:5994";
-function getPicture() {
-  id = Math.floor((Math.random() * 118));
-  picture = Math.floor((Math.random() * 50));
-  $('#art').attr('src', 'http://artchallenge.me/painters/' + id + '/' + picture + '.jpg');
-}
-
-function answer(btn){
-  if(btn==1){wrongAnswer();}
-  if(btn==2){rightAnswer();}
-  if(btn==3){wrongAnswer();}
-  if(btn==4){rightAnswer();}
-  $('#screenQuestion').css('display','none');
-  window.generatingNewRound=false;
-}
-
-function rightAnswer() {
-  // $('#screenRight').animateCss('fadeIn');
-  $('#screenRight').css('display', 'block');
-  setTimeout(function () {
+$.couch.db("painters").allDocs({
+  include_docs: true,
+  success: function(data) {
+    // console.log(data);
+    painters = data.rows;
+    $('#divButtons').css('display', 'block');
     newRound();
-  }, 5000);
+  }
+});
+
+$.couch.db("lang").allDocs({
+  include_docs: true,
+  success: function(data) {
+    // console.log(data);
+    langDB = data.rows;
+  }
+});
+
+function randomPainter() {
+  return Math.floor((Math.random() * Object.keys(painters).length));
 }
 
-function wrongAnswer() {
-  // $('#screenWrong').animateCss('fadeIn');
-  $('#screenWrong').css('display', 'block');
-  // setTimeout(function () {
-  //   newRound();
-  // }, 3000);
+function getRandomPicture() {
+  id = randomPainter();
+  currentPainter = painters[id].doc;
+  console.log(currentPainter);
+  picture = Math.floor((Math.random() * Object.keys(currentPainter.paintings).length )) + 1;
+  $('#art').attr('src', 'http://artchallenge.me/painters/' + currentPainter._id + '/' + picture + '.jpg');
+
+  if(currentPainter.paintings[picture].name[Lang]){
+    $('#title').html(currentPainter.paintings[picture].name[Lang]);
+    $('#year').html(" "+currentPainter.paintings[picture].year+ " ");
+  }
+
+  $('.currentName').html(currentPainter.name);
+  $('.bioMini').html(currentPainter.bio[Lang].slice(0,100));
+  $('.bio').html(currentPainter.bio[Lang].slice(100,-1));
 }
 
 function newRound() {
@@ -36,23 +51,80 @@ function newRound() {
     $('#screenWrong').css('display', 'none');
     $('#screenRight').css('display', 'none');
     $('#screenQuestion').css('display', 'block');
-    $('#screenQuestion').animateCss('bounceInUp');
-    getPicture();
-
-    $('html, body').animate({
-      scrollTop: $("#divGame").offset().top
-    }, 500);
-
+    // $('#screenQuestion').animateCss('bounceInUp');
+    getRandomPicture();
+    generateAnswers();
+    $('html, body').animate({scrollTop: $("#divGame").offset().top-100}, 500);
   }
 }
 
-newRound();
+function generateAnswers() {
+  answers=[];
+  answers.push(currentPainter);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+  answers.push(painters[randomPainter()].doc);
+
+  //unique
+  answers = answers.reverse().filter(function(e, i, answers) {
+    return answers.indexOf(e, i + 1) === -1;
+  }).reverse();
+
+  answers = answers.slice(0,4);
+  shuffle(answers);
+
+  for (i = 1; i <= 4; i++) {
+    $('#btn-'+i+'-text').html(answers[i-1].name);
+    $('#btn-'+i+'-img').attr("src","http://artchallenge.me/painters/" + answers[i-1]._id + "/photo.jpg");
+    $('#btn'+i).attr("painterId",answers[i-1]._id);
+  }
+}
+
+function answer(btn){
+  if ($('#btn'+btn).attr("painterId") == currentPainter._id) {
+    rightAnswer();
+    ++scores;
+    $("#star"+scores).css("color","green").attr('class', 'fa fa-star stars');
+
+  } else {
+    wrongAnswer();
+    scores = 0;
+    $(".stars").css("color","#6c7279").attr('class', 'fa fa-star-o stars');
+
+  }
+  $('#screenQuestion').css('display','none');
+  window.generatingNewRound=false;
+}
+
+function rightAnswer() {
+  // $('#screenRight').animateCss('fadeIn');
+  $('#screenRight').css('display', 'block');
+  $('#rightPhoto').attr('src', 'http://artchallenge.me/painters/'+currentPainter._id+'/photo.jpg');
+
+  // setTimeout(function () {newRound();}, 5000);
+}
+
+function wrongAnswer() {
+  // $('#screenWrong').animateCss('fadeIn');
+  $('#screenWrong').css('display', 'block');
+  $('#wrongPhoto').attr('src', 'http://artchallenge.me/painters/'+currentPainter._id+'/photo.jpg');
+
+  // setTimeout(function () {newRound();}, 5000);
+}
 
 // $.couch.db("painters").openDoc(window.truePainter.toString(), {
 //         success: function(data) {
 //
 //
-//       data.paintings[window.image.id-1].name[window.lang] = pictureName;
+//       data.paintings[window.image.id-1].name[window.langDB] = pictureName;
 //       data.paintings[window.image.id-1].year = pictureYear;
 //
 //       // console.log(data);
@@ -69,12 +141,3 @@ newRound();
 //     swal("Ошибка", "Произошла ошибка при подключении к базе данных. Пожалуйста, обратитесь в техподдержку", 'error');
 //   }
 // });
-
-$.couch.db("painters").allDocs({
-  include_docs: true,
-  // descending: true,
-  // limit: 10,
-  success: function(data) {
-    console.log(data);
-  }
-});
