c16bf8113332aad50b4e59a3847761ad5ddc2f76
Ruben <just14zy@gmail.com>
2022-06-14 17:47:38 +0300

    beget

--- js/engine.js
+++ js/engine.js
@@ -104,7 +104,7 @@ function load() {
   }
 
 
-  window.platform = "https://images.weserv.nl/?url=http://178.62.133.139/painters/"; //"http://artchallenge.me/painters/"; // "painters/"; //http://178.62.133.139/painters/1/1.jpg; //images.weserv.nl/?url=http://178.62.133.139/painters/119/1.jpg
+  window.platform = "https://images.weserv.nl/?url=http://rybikyem.beget.tech/painters/"; //http://178.62.133.139/painters/ //"http://artchallenge.me/painters/"; // "painters/"; //http://178.62.133.139/painters/1/1.jpg; //images.weserv.nl/?url=http://178.62.133.139/painters/119/1.jpg
   document.cookie = "wins=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
 
   window.errorDelay = 3000;
