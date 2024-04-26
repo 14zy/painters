36d708c431d5266fe8aae5ec741f9238ca05dbe2
Ruben <just14zy@gmail.com>
2024-04-26 20:05:36 +0700

    pic change tmp

--- js/engine.js
+++ js/engine.js
@@ -102,10 +102,10 @@ function load() {
     lang = "en";
     setLang(lang);
   }
-
+  
   
 
-  window.platform = "https://res.cloudinary.com/drknuk7m5/image/upload/ac/"; //"https://res.cloudinary.com/daxsqgruu/image/upload/artchallenge/" //"https://ik.imagekit.io/artchallenge/"; //"https://artchallenge.ru/pictures/"; //"https://ik.imagekit.io/artchallenge/"; //http://178.62.133.139/painters/ //"http://artchallenge.me/painters/"; // "painters/"; //http://178.62.133.139/painters/1/1.jpg; //images.weserv.nl/?url=http://178.62.133.139/painters/119/1.jpg //https://ik.imagekit.io/artchallenge/21/152.jpg //https://cdn.imgproxify.com/image?url=http://rybikyem.beget.tech/painters/54/250-test.jpg
+  window.platform = "https://artchallenge.world/pics/paintings/"; //"https://res.cloudinary.com/drknuk7m5/image/upload/ac/"; "https://res.cloudinary.com/daxsqgruu/image/upload/artchallenge/" //"https://ik.imagekit.io/artchallenge/"; //"https://artchallenge.ru/pictures/"; //"https://ik.imagekit.io/artchallenge/"; //http://178.62.133.139/painters/ //"http://artchallenge.me/painters/"; // "painters/"; //http://178.62.133.139/painters/1/1.jpg; //images.weserv.nl/?url=http://178.62.133.139/painters/119/1.jpg //https://ik.imagekit.io/artchallenge/21/152.jpg //https://cdn.imgproxify.com/image?url=http://rybikyem.beget.tech/painters/54/250-test.jpg
   document.cookie = "wins=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
 
   window.errorDelay = 3000;
