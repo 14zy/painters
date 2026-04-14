f1f7d4834ba4dd6bfab7d634e14a602d8f0ad0c1
Ruben <just14zy@gmail.com>
2026-04-14 15:25:22 +0700

    domain update 2

--- js/engine.js
+++ js/engine.js
@@ -610,7 +610,7 @@ function goodPhrase() {
 function winner() {
 
   window.rndShare = Math.floor((Math.random() * 10) + 1);
-  window.imageURL = "https://artchallenge.world/pics/shares/" + window.currentSetName.slice(0, -3) + "/" + window.lang + "/" + window.rndShare + ".jpg";
+  window.imageURL = "https://art.rubeton.app/pics/shares/" + window.currentSetName.slice(0, -3) + "/" + window.lang + "/" + window.rndShare + ".jpg";
 
   var winnerDiv = "\
   <p class='winner-text'>" + i18n.t("message.winner-desc", {
@@ -703,7 +703,7 @@ function getShares() {
 }
 
 function ShareFB() {
-  url = "https://www.facebook.com/dialog/feed?app_id=478531102278887&display=popup&link=https://artchallenge.world/?utm_source=fb-win&redirect_uri=https://artchallenge.world/1.html&picture=" + "https://artchallenge.world/share.jpg" + "&source=" + "https://artchallenge.world/share.jpg"+"&name=" + i18n.t("shares.title", {
+  url = "https://www.facebook.com/dialog/feed?app_id=478531102278887&display=popup&link=https://art.rubeton.app/?utm_source=fb-win&redirect_uri=https://art.rubeton.app/1.html&picture=" + "https://art.rubeton.app/share.jpg" + "&source=" + "https://art.rubeton.app/share.jpg"+"&name=" + i18n.t("shares.title", {
     lng: window.lang
   }) + "&caption=" + i18n.t("shares.caption", {
     lng: window.lang
@@ -724,11 +724,11 @@ function ShareFB() {
 
 function ShareVK() {
 
-  url = "https://vk.com/share.php?url=https://artchallenge.world/?utm_source=vk-win&title=" + i18n.t("shares.description", {
+  url = "https://vk.com/share.php?url=https://art.rubeton.app/?utm_source=vk-win&title=" + i18n.t("shares.description", {
     lng: window.lang
   }) + " %23ArtChallenge&description=" + i18n.t("shares.caption", {
     lng: window.lang
-  }) + "&image=" + "https://artchallenge.world/share.jpg" + "&noparse=true";
+  }) + "&image=" + "https://art.rubeton.app/share.jpg" + "&noparse=true";
   window.open(url, 'targetWindow', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=604,height=401');
   yaCounter24594722.reachGoal('WINNER-SHARE-VK');
 }
